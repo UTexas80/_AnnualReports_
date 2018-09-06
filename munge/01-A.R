4f72a043220c9b38d9bc864274d3de613ac8ede3
UTexas80 <glen.falk@utexas.edu>
2018-09-06 07:47:22 -0400

    20180906AnnualReportsA
    a:  Added munge01
    b.  Updated Config file

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,6 @@
 # Example preprocessing script.
+h <- read_html("https://tinyurl.com/ybjbdpp3")
+reps <- h %>%
+    html_node("#mw-content-text > div > table:nth-child(18)") %>%
+    html_table()
+reps <- reps[,c(1:2,4:9)] %>% as_tibble()
