d9dc4de9c7d4d71230e0f2df3572e7b880de9de9
UTexas80 <glen.falk@utexas.edu>
2018-09-07 14:18:06 -0400

    20180907AnnualReportsA
    a. config.dcf: update libraries
    b. tbl_test: Test URL load
    c. globals.R: add ay, currentYr

--- lib/globals.R
+++ lib/globals.R
@@ -2,7 +2,10 @@
 add.config(
   apply.override = FALSE
 )
-
+currentYr <- as.numeric(format(Sys.Date(), format="%y"))
+lastYr <- currentYr - 1
+currentAY <- as.numeric(paste(lastYr, currentYr, sep = ""))
+ay<-as.data.table(seq(as.numeric(currentAY), as.numeric(currentAY)+(101*(100-currentYr-1)),by=101))
 # Add project specific configuration that can be overridden from load.project()
 add.config(
   apply.override = TRUE
